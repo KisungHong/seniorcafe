49fec9d4800806a01640dbaa24b7debc760a4f36
kisung <ppter0311@naver.com>
2023-03-09 22:08:45 +0900

    2. Page UI : lesson(2)

---
+++ app/src/main/java/com/example/seniorcafe/Lesson.kt
@@ -0,0 +1,7 @@
+package com.example.seniorcafe
+
+data class Lesson(
+    var lessonTitle : String? = "",
+    var teacher : String? = "",
+    var lessonCoverImg : Int? = null
+)
--- app/src/main/java/com/example/seniorcafe/LessonFragment.kt
+++ app/src/main/java/com/example/seniorcafe/LessonFragment.kt
@@ -1,6 +1,7 @@
 package com.example.seniorcafe
 
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Albums
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import com.example.seniorcafe.databinding.FragmentLessonBinding
 class LessonFragment : Fragment() {
 
     lateinit var binding : FragmentLessonBinding
+    private var lessonData = ArrayList<Lesson>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +20,12 @@ class LessonFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLessonBinding.inflate(inflater,container,false)
+
+        lessonData.apply {
+            add(Lesson("첫번째 레슨","첫번째 선생님",R.drawable.cook1))
+            add(Lesson("첫번째 레슨","첫번째 선생님",R.drawable.cook1))
+        }
+
         return binding.root
     }
 
